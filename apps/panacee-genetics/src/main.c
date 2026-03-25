5183e455776a89888ede15f2317f3479241d66ed
Pierre Guéroult <pierregueroult2022@gmail.com>
2026-03-25 09:02:27 +0100

    chore(mlv): add mlv library to make file

--- apps/panacee-genetics/src/main.c
+++ apps/panacee-genetics/src/main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+#include <MLV/MLV_all.h>
+
 int main(void)
 {
-    puts("Panacée: projet C initialisé.");
+    MLV_create_window("Panacée Genetics", "hello world", 640, 480);
+    MLV_wait_seconds(10);
+    MLV_free_window();
     return 0;
 }
